798f1b7acd732965cd67b32ffb8a21168bd39106
Suman H E <suman.he@digitusbiz.com>
2024-10-24 15:11:34 +0530

    Added Amphenol Logo

---
+++ src/components/Logo/logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Logod from './amphenol_logo.png';
+
+function Logo() {
+  return (
+    <img src={Logod} alt="Logo" className="logo-image" />
+  );
+}
+
+export default Logo;
