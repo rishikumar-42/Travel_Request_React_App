40b1bbfde1efa582b11224174b1284d4e2bce298
RISHIKUMAR <rishikumar.s@digitusbiz.com>
2025-02-04 17:07:45 +0530

    add Amphenol footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import "../assets/css/Footer.css";
 function Footer() {
     return (
         <footer className="footer">
-            <p>&copy; 2025 Digitus Business Solutions</p>
+            <p>&copy; 2025 Amphenol. All rights reserved. Version 1.0</p>
         </footer>
     );
 }
